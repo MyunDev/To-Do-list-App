1bd619bfb53aa0bed53a9b3a5bc464375340130c
김현기 <kimhyunki@gimhyeongiui-MacBookAir.local>
2020-02-28 17:50:28 +0900

    Making To Do list

--- App.js
+++ App.js
@@ -1,19 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform, ScrollView } from 'react-native';
+import { render } from 'react-dom';
+import ToDo from "./ToDo";
 
-
-const {height, width} = Dimensions.get("window");
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle ="light-content"/>
-      <Text style={styles.title}>To Do List</Text>
-      <View style={styles.card}>
-        <TextInput style={styles.input} placeholder={"New To Do"}/>
+const { height, width } = Dimensions.get("window");
+export default class App extends React.Component {
+  state = {
+    newToDo: ""
+  }
+  render() {
+    const { newToDo } = this.state;
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>To Do List</Text>
+        <View style={styles.card}>
+          <TextInput style={styles.input}
+            placeholder={"New To Do"}
+            value={newToDo}
+            onChangeText={this._controllNewToDo}
+            placeholderTextColor={"#999"}
+            returnKeyType={"done"}
+            autoCorrect={false}
+          />
+          <ScrollView>
+            <ToDo />
+          </ScrollView>
+        </View>
       </View>
-    </View>
-  );
+    );
+  }
+  _controllNewToDo = text => {
+    //이벤트에서 텍스트를 가져옴 
+    this.setState({
+      newToDo: text
+    });
+  };
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -36,18 +60,24 @@ const styles = StyleSheet.create({
     borderTopLeftRadius: 10,
     borderTopRightRadius: 10,
     ...Platform.select({
-      ios:{
-        shadowColor:"rgb(50,50,50)",
+      ios: {
+        shadowColor: "rgb(50,50,50)",
         shadowOpacity: 0.5,
         shadowRadius: 5,
-        shadowOffset:{
+        shadowOffset: {
           height: -1,
           width: 0
         }
       },
-      android:{
+      android: {
         elecation: 3
       }
     })
+  },
+  input: {
+    padding: 20,
+    borderBottomColor: "#bbb",
+    borderBottomWidth: 1,
+    fontSize: 20
   }
 });
